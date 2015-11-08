83096dfaf3525f34881948e9570a9248c18943a2
JNajjar <johannes.najjar@tngtech.com>
2015-11-08 17:03:08 +0100

    additional tests

--- libraries/lib-datahandler/src/test/java/com/siemens/sw360/datahandler/permissions/ProjectPermissionsTest.java
+++ libraries/lib-datahandler/src/test/java/com/siemens/sw360/datahandler/permissions/ProjectPermissionsTest.java
@@ -22,24 +22,83 @@ import com.siemens.sw360.datahandler.permissions.jgivens.ThenHighestAllowedActio
 import com.siemens.sw360.datahandler.permissions.jgivens.WhenComputePermissions;
 import com.siemens.sw360.datahandler.thrift.users.RequestedAction;
 import com.siemens.sw360.datahandler.thrift.users.UserGroup;
+import com.tngtech.java.junit.dataprovider.DataProvider;
+import com.tngtech.java.junit.dataprovider.DataProviderRunner;
+import com.tngtech.java.junit.dataprovider.UseDataProvider;
 import com.tngtech.jgiven.junit.ScenarioTest;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static com.siemens.sw360.datahandler.thrift.users.RequestedAction.*;
+import static com.siemens.sw360.datahandler.thrift.users.UserGroup.*;
 
 /**
  * @author johannes.najjar@tngtech.com
  */
+
+@RunWith(DataProviderRunner.class)
 public class ProjectPermissionsTest extends ScenarioTest<GivenProject, WhenComputePermissions, ThenHighestAllowedAction> {
 
     public static String theUser = "user1";
     public static String theOtherUser = "anotherUser";
 
+
+    /**
+     * See
+     * com.siemens.sw360.datahandler.permissions.DocumentPermissions.getHighestAllowedPermission()
+     * for relevant cases
+     */
+    @DataProvider
+    public static Object[][] highestAllowedActionProvider() {
+        // @formatter:off
+        return new Object[][] {
+                { theUser, theUser, USER, CLEARING },
+                { theUser, theOtherUser, USER, READ },
+                { theUser, theOtherUser, CLEARING_ADMIN, ATTACHMENTS },
+                { theUser, theOtherUser, ADMIN, CLEARING },
+        };
+        // @formatter:on
+    }
+
     @Test
-    public void test000() throws Exception {
-        given().a_project_created_by_$(theUser);
-        when().the_highest_allowed_action_is_computed_for_user_$_with_user_group_$(theUser,  UserGroup.USER);
-        then().the_highest_allowed_action_should_be(RequestedAction.CLEARING);
+    @UseDataProvider("highestAllowedActionProvider")
+    public void testHighestAllowedActionCreatedBy(String createdBy, String requestingUser, UserGroup requestingUserGroup, RequestedAction highestAllowedAction) throws Exception {
+        given().a_project_created_by_$(createdBy);
+        when().the_highest_allowed_action_is_computed_for_user_$_with_user_group_$(requestingUser, requestingUserGroup);
+        then().the_highest_allowed_action_should_be(highestAllowedAction);
+    }
+
+    @Test
+    public void testHighestAllowedActionLeadArchitect() throws Exception {
+        given().a_project_with_lead_architect_$(theUser);
+        when().the_highest_allowed_action_is_computed_for_user_$_with_user_group_$(theUser, USER);
+        then().the_highest_allowed_action_should_be(ATTACHMENTS);
     }
 
 
-    //TODO write much more tests
+// Now it would be easy to write tests with copy and paste, but lets see...
+//    @Test
+//    public void testHighestAllowedActionModerator() throws Exception {
+//        given().a_project_with_moderator_$(theUser);
+//        when().the_highest_allowed_action_is_computed_for_user_$_with_user_group_$(theUser, USER);
+//        then().the_highest_allowed_action_should_be(CLEARING);
+//    }
+//
+//    @Test
+//    public void testHighestAllowedActionCoModerator() throws Exception {
+//        given().a_project_with_comoderator_$(theUser);
+//        when().the_highest_allowed_action_is_computed_for_user_$_with_user_group_$(theUser, USER);
+//        then().the_highest_allowed_action_should_be(CLEARING);
+//    }
+//
+//    @Test
+//    public void testHighestAllowedActionContributor() throws Exception {
+//        given().a_project_with_contributor_$(theUser);
+//        when().the_highest_allowed_action_is_computed_for_user_$_with_user_group_$(theUser, USER);
+//        then().the_highest_allowed_action_should_be(ATTACHMENTS);
+//    }
+
+
+
+
 }
