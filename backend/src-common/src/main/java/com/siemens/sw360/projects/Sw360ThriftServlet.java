fb7e18491efdb566b8027321a0c6d32834f91ccb
Birgit Heydenreich <birgit.heydenreich@tngtech.com>
2015-12-22 16:04:28 +0100

    added header to sw360ThriftServlet added header to sw360ThriftServlet

--- backend/src-common/src/main/java/com/siemens/sw360/projects/Sw360ThriftServlet.java
+++ backend/src-common/src/main/java/com/siemens/sw360/projects/Sw360ThriftServlet.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright Siemens AG, 2015. Part of the SW360 Portal Project.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License Version 2.0 as published by the
+ * Free Software Foundation with classpath exception.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License version 2.0 for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program (please see the COPYING file); if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
 package com.siemens.sw360.projects;
 
 import org.apache.log4j.Logger;
@@ -13,7 +30,7 @@ import java.io.IOException;
 import static org.apache.log4j.Logger.getLogger;
 
 /**
- * Created by andreas on 27.11.15.
+ * @author Andreas.Reichel@tngtech.com
  */
 public class Sw360ThriftServlet extends TServlet {
     private static final Logger log = getLogger(Sw360ThriftServlet.class);
