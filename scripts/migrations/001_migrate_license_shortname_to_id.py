5ee149d0cbcf85436446b5413a01cc37b07e2974
Alex Borodin <alex.borodin@evosoft.com>
2016-04-12 10:28:24 +0200

    fixed db migration script

--- scripts/migrations/001_migrate_license_shortname_to_id.py
+++ scripts/migrations/001_migrate_license_shortname_to_id.py
@@ -1,9 +1,29 @@
 #!/usr/bin/python
-
-
-# This is a manual database migration script. It is assumed that a dedicated migration framework will be written in the future.
+# -----------------------------------------------------------------------------
+# Copyright Siemens AG, 2016. Part of the SW360 Portal Project.
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License Version 2.0 as published by the
+# Free Software Foundation with classpath exception.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License version 2.0 for
+#  more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program (please see the COPYING file); if not, write to the Free
+# Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.
+#
+#
+# This is a manual database migration script. It is assumed that a dedicated framework for automatic migration will be written in the future.
 # When that happens, this script should be refactored to conform to the framework's prerequisites to be run by the framework.
 # For example, server address and db name should be parametrized, the code reorganized into a single class or function, etc.
+#
+# initial author: alex.borodin@evosoft.com
+#
+# -----------------------------------------------------------------------------
 
 
 import couchdb
@@ -43,6 +63,6 @@ for license_row in licenses:
     license = license_row.value
     db.delete(license)
     del license['_rev']
-    del license['shortname']
     license['_id'] = license['shortname']
+    del license['shortname']
     db.save(license)
