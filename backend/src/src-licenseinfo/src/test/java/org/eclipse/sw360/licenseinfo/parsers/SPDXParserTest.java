c8c859de647d340a641c799271cf70d574bb67b2
Maximilian Huber <external.maximilian.huber2@bosch-si.com>
2017-02-02 13:04:58 +0100

    fix(spdx): `getAllLicenseTexts` was not called due to merge failure
    Signed-off-by: Maximilian Huber <external.maximilian.huber2@bosch-si.com>

--- backend/src/src-licenseinfo/src/test/java/org/eclipse/sw360/licenseinfo/parsers/SPDXParserTest.java
+++ backend/src/src-licenseinfo/src/test/java/org/eclipse/sw360/licenseinfo/parsers/SPDXParserTest.java
@@ -65,7 +65,7 @@ public class SPDXParserTest {
         assertThat(result.getFilenames().size(), is(1));
         assertThat(result.getFilenames().get(0), is(spdxExampleFile));
 
-        assertThat(result.getLicenseNamesWithTextsSize(), is(5));
+        assertThat(result.getLicenseNamesWithTextsSize(), is(7));
         assertThat(result.getLicenseNamesWithTexts()
                 .stream()
                 .map(lt -> lt.getLicenseText())
