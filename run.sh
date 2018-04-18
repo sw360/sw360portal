e6a9ae195a7cec06d5a7a55ae98e2d9c225c7ada
Alex Skorohod <alexey.skorohod@gmail.com>
2018-04-18 08:27:10 +0300

    AutoFill on component adding page

--- run.sh
+++ run.sh
@@ -20,5 +20,6 @@ cp -rf portal-ext.properties ${LIFERAY_PATH}/tomcat-7.0.62/webapps/ROOT/WEB-INF/
 mvn clean install -DskipTests -Pdeploy -Ddeploy.dir=${LIFERAY_PATH}/deploy -Dwebapps.dir=${LIFERAY_PATH}/tomcat-7.0.62/webapps
 
 echo ">>> Starting Tomcat of Liferay"
-set JPDA_OPTS="-agentlib:jdwp=transport=dt_socket, address=5005, server=y, suspend=n"
-${LIFERAY_PATH}/tomcat-7.0.62/bin/catalina.sh jpda start
+#set CATALINA_OPTS="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005"
+#set in setenv.sh
+${LIFERAY_PATH}/tomcat-7.0.62/bin/catalina.sh run
