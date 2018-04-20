9f23f6e88dbe5c144c3294e3a0aae44c1cee4979
NazariiMed <nazar.medykh@gmail.com>
2018-04-20 18:09:51 +0300

    final fixes

--- docker/couchdb-setup.sh
+++ docker/couchdb-setup.sh
@@ -2,9 +2,6 @@
 
 bash -c 'sleep 20s && curl -H "Content-Type: application/json" -X POST -d '"'"'{"action":"enable_single_node","password":"admin","port":"5984","singlenode":"true","username":"admin"}'"'"' http://localhost:5984/_cluster_setup' &
 disown
-echo "$1"
 set -- /docker-entrypoint.sh "$@"
-echo "$@"
 
 exec "$@"
-
